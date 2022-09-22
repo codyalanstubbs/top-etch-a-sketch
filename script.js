49cbaaf15b25b08f3a4df93c5a50eeb074f56dd5
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-22 16:06:59 -0400

    Create buildGrid() to build initial and new grids
    Ended up creating a function for building the grid since I
    started out by just copying the original for loop. The
    function also removes previous squares and sets event
    listeners for coloring.

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-let sketchy = document.querySelector('.sketchy');
-
-for (let i = 1; i <= (16*16); i++) {
-    
-    let sketchySquare = document.createElement('div');
-
-    sketchySquare.id = i;
-    sketchySquare.classList.add("square");
-
-    sketchy.appendChild(sketchySquare);
-}
-
-let squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        console.log(square);
-        console.log(e);
-        square.style.background = "pink";
-    })
-})
+let numberSquares = 16;
+buildGrid(numberSquares);
 
 let dimensionsButton = document.querySelector('.editDimensions');
 dimensionsButton.addEventListener('click', (e) => {
-    
+
     let numberSquares = prompt("Enter the number of squares per side (max 100): ");
     checkLessThan101(numberSquares);
+    buildGrid(numberSquares);
+
+})
+
+function buildGrid(numberSquares) {
+
+    let oldSquares = document.querySelectorAll('.square');
+    if (!!oldSquares) { // If old squares exist, then remove them
+        oldSquares.forEach((square) => {
+            square.remove();
+        })
+    }
     
-    for (let i = 1; i <= (numberSquares*numberSquares); i++) {
-        sketchy.remove();
-        sketchy = document.createElement('.sketchy');
-        document.querySelector('body').appendChild(sketchy)
+    // Create new squares
+    for (let i = 1; i <= (numberSquares ** 2); i++) {
+        sketchy = document.querySelector('div');
+        sketchy.classList.add('sketchy');
 
         let sketchySquare = document.createElement('div');
-  
+
         sketchySquare.id = i;
         sketchySquare.classList.add("square");
-        sketchySquare.style.width = 320 /numberSquares;
-        sketchySquare.style.height = 320 /numberSquares;
-    
+        sketchySquare.style.width = 320 / numberSquares + 'px';
+        sketchySquare.style.height = 320 / numberSquares + 'px';
+
         sketchy.appendChild(sketchySquare);
     }
-})
+
+    // Add event listeners for coloring squares
+    let newSquares = document.querySelectorAll('.square');
+    newSquares.forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            square.style.background = "pink";
+        })
+    })
+}
 
 function checkLessThan101(numberSquares) {
     if (numberSquares > 100) {
