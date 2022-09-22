5bf61c264eb8db7cce6500843ce317c875db20b8
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-22 15:04:16 -0400

    Add borders to grid

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let sketchy = document.querySelector('.sketchy');
 
 for (let i = 1; i <= (16*16); i++) {
+    
     let square = document.createElement('div');
+
+    square.id = i;
     square.classList.add("square");
+
     sketchy.appendChild(square);
 }
