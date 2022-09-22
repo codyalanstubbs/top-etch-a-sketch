3422a722cf34a2d547af9204e5de71000c4e9b8f
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-22 15:12:16 -0400

    Color square pink when hovering over it

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let sketchy = document.querySelector('.sketchy');
 
 for (let i = 1; i <= (16*16); i++) {
     
-    let square = document.createElement('div');
+    let sketchySquare = document.createElement('div');
 
-    square.id = i;
-    square.classList.add("square");
+    sketchySquare.id = i;
+    sketchySquare.classList.add("square");
 
-    sketchy.appendChild(square);
+    sketchy.appendChild(sketchySquare);
 }
+
+let squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', (e) => {
+        console.log(square);
+        console.log(e);
+        square.style.background = "pink";
+    })
+})
