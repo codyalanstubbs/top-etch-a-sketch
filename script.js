dec181a466b5754a66651fc1611448a0d64bc506
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-22 15:36:34 -0400

    Add button and prompt for dynamic grid dimensions

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ squares.forEach((square) => {
         square.style.background = "pink";
     })
 })
+
+let dimensionsButton = document.querySelector('.editDimensions');
+dimensionsButton.addEventListener('click', (e) => {
+    
+    let numberSquares = prompt("Enter the number of squares per side (max 100): ");
+    checkLessThan101(numberSquares);
+    
+    for (let i = 1; i <= (numberSquares*numberSquares); i++) {
+        sketchy.remove();
+        sketchy = document.createElement('.sketchy');
+        document.querySelector('body').appendChild(sketchy)
+
+        let sketchySquare = document.createElement('div');
+  
+        sketchySquare.id = i;
+        sketchySquare.classList.add("square");
+        sketchySquare.style.width = 320 /numberSquares;
+        sketchySquare.style.height = 320 /numberSquares;
+    
+        sketchy.appendChild(sketchySquare);
+    }
+})
+
+function checkLessThan101(numberSquares) {
+    if (numberSquares > 100) {
+        let numberSquares = prompt(`Enter the number of squares per side (LESS THAN 101): `)
+        checkLessThan101(numberSquares);
+    };
+}
